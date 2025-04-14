c59f58c4d47c458ad9a6f9252aefd5d0ceb56509
Monster <bf.monster@proton.me>
2025-04-14 20:10:26 +0200

    fix: update login methods in Privy config to include telegram

--- src/config/privy.ts
+++ src/config/privy.ts
@@ -2,6 +2,6 @@ import type { PrivyClientConfig } from '@privy-io/react-auth';
 import { polygon, polygonAmoy } from 'wagmi/chains';
 // Replace this with your Privy config
 export const privyConfig: PrivyClientConfig = {
-	loginMethods: ['passkey', 'email', 'google', 'apple', 'sms'],
+	loginMethods: ['passkey', 'email', 'google', 'sms', 'telegram'],
 	supportedChains: [polygon, polygonAmoy],
 };
