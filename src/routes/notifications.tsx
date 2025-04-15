fe48e282bc25c46c2e99560a31fed8a5d43dc8a2
Monster <bf.monster@proton.me>
2025-04-15 18:24:25 +0200

    feat: added noitification button

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -1,5 +1,18 @@
+import { Button } from '@betfinio/components';
+
 function Notifications() {
-	return <div>Notifications</div>;
+	const handleAllowNotifications = () => {
+		Notification.requestPermission().then((permission) => {
+			if (permission === 'granted') {
+				console.log('notifications allowed');
+			}
+		});
+	};
+	return (
+		<div>
+			<Button onClick={handleAllowNotifications}>Allow notifications</Button>
+		</div>
+	);
 }
 
 export default Notifications;
