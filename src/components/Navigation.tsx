8c1905aa4f928a174f31b1f7f6709126c7475031
Monster <bf.monster@proton.me>
2025-04-17 16:10:40 +0200

    feat: add active state for navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,15 @@
 import { useSidebar } from '@betfinio/components/ui';
-import { Link } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 import { BellIcon, Gamepad2Icon, Layers3Icon, MenuIcon, WalletIcon } from 'lucide-react';
 
 function Navigation() {
 	const { toggleSidebar } = useSidebar();
+
+	const { pathname } = useLocation();
+
+	const isActive = (href: string) => {
+		return pathname.includes(href);
+	};
 
 	return (
 		<nav className="grid grid-cols-5 fixed w-screen bottom-0 left-0 border-t border-border p-2 pt-3 bg-background h-16 z-10">
@@ -11,19 +17,35 @@ function Navigation() {
 				<MenuIcon className="w-6 h-6" />
 				Menu
 			</div>
-			<Link to="/app" className="flex flex-col items-center justify-center text-xs">
+			<Link
+				to="/app"
+				data-active={isActive('/app')}
+				className="flex flex-col items-center justify-center transition-all duration-300 text-xs data-[active=true]:text-primary data-[active=true]:scale-110"
+			>
 				<Gamepad2Icon className="w-6 h-6" />
 				Lobby
 			</Link>
-			<Link to="/staking" className="flex flex-col items-center justify-center text-xs ">
+			<Link
+				to="/staking"
+				data-active={isActive('/staking')}
+				className="flex flex-col items-center justify-center transition-all duration-300 text-xs data-[active=true]:text-primary data-[active=true]:scale-110"
+			>
 				<Layers3Icon className="w-6 h-6" />
 				Staking
 			</Link>
-			<Link to="/notifications" className="flex flex-col items-center justify-center text-xs">
+			<Link
+				to="/notifications"
+				data-active={isActive('/notifications')}
+				className="flex flex-col items-center justify-center transition-all duration-300 text-xs data-[active=true]:text-primary data-[active=true]:scale-110"
+			>
 				<BellIcon className="w-6 h-6" />
 				Notifications
 			</Link>
-			<Link to="/wallet" className="flex flex-col items-center justify-center text-xs">
+			<Link
+				to="/wallet"
+				data-active={isActive('/wallet')}
+				className="flex flex-col items-center justify-center transition-all duration-300 text-xs data-[active=true]:text-primary data-[active=true]:scale-110"
+			>
 				<WalletIcon className="w-6 h-6" />
 				Wallet
 			</Link>
